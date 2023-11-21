ab2c5ad5e2fc261bcf22aa50b14543a34bf52c36
Xuanru Zhou <108257092+rorizzz@users.noreply.github.com>
2023-11-21 10:18:10 +0800

    Delete vits directory

--- vits/try.py
+++
@@ -1,5 +0,0 @@
-from phonemizer import phonemize
-
-text = "Please ...call stella"
-phonemes = phonemize(text, language='en-us', backend='espeak', strip=True, preserve_punctuation=True, with_stress=True)
-print(phonemes)  # str
